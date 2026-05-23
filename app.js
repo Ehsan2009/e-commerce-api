6a28ca8b6777827f4c1fd8a8d87f1562ea1be274
Ehsan Javdan <ehsanjavdan77@gmail.com>
2026-05-23 22:22:58 +0400

    i satup a based express server with adding some packages like morgan and also error handlers and some middlewares

---
+++ app.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+
+// express
+const express = require("express");
+const app = express();
+
+// rest of the packages
+const morgan = require("morgan")
+
+// database
+const connectDB = require("./db/connect");
+
+// middleware
+const notFoundMiddleware = require("./middleware/not_found");
+const errorHandlerMiddleware = require("./middleware/error_handler");
+
+app.use(morgan("tiny"))
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("ecommerce api");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ errors/index.js
@@ -0,0 +1,10 @@
+const CustomAPIError = require('./custom_api');
+const UnauthenticatedError = require('./unauthenticated');
+const NotFoundError = require('./not_found');
+const BadRequestError = require('./bad_request');
+module.exports = {
+  CustomAPIError,
+  UnauthenticatedError,
+  NotFoundError,
+  BadRequestError,
+};
