e5bfd0fce2bf16428c80a3e5044f2454b6dcc3d4
Ehsan Javdan <ehsanjavdan77@gmail.com>
2026-05-25 22:48:42 +0400

    worked on register controller, in that controller i created the user and fixed doublicate email issue and also worked on admin logic so that the first user of db is going to be the admin and secured the admin role

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,5 +1,22 @@
-const register = (req, res) => {
-  res.send("register user");
+const User = require("../models/user");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const emailAlreadyExists = await User.findOne({ email });
+
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError("Email already exists");
+  }
+
+  // first register user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user"
+
+  user = await User.create({ name, email, password, role });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = (req, res) => {
