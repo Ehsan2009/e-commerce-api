170597dd4b23ef3e8d3cfb6ae769c74871305e5c
Ehsan Javdan <ehsanjavdan77@gmail.com>
2026-05-30 21:47:17 +0400

    structured the user controller and routes

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const app = express();
 // rest of the packages
 const morgan = require("morgan")
 const cookieParser = require("cookie-parser")
+const cors = require("cors")
 
 // database
 const connectDB = require("./db/connect");
 
 // routers
 const authRouter = require("./routes/auth_routes")
+const userRouter = require("./routes/user_routes")
 
 // middleware
 const notFoundMiddleware = require("./middleware/not_found");
@@ -21,12 +23,14 @@ const errorHandlerMiddleware = require("./middleware/error_handler");
 app.use(morgan("tiny"))
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET))
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("ecommerce api");
 });
 
 app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/users", userRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
