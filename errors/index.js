f83bdd60da5aa9de6394b86ae874edd425b3427b
Ehsan Javdan <ehsanjavdan77@gmail.com>
2026-06-01 22:51:34 +0400

    completed authorize permissions middleware

--- errors/index.js
+++ errors/index.js
@@ -2,9 +2,12 @@ const CustomAPIError = require('./custom_api');
 const UnauthenticatedError = require('./unauthenticated');
 const NotFoundError = require('./not_found');
 const BadRequestError = require('./bad_request');
+const UnauthorizedError = require('./unauthorized');
+
 module.exports = {
   CustomAPIError,
   UnauthenticatedError,
   NotFoundError,
   BadRequestError,
+  UnauthorizedError
 };
