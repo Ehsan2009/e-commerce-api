0d744b5e38b003fda2a8127e0052c6f5be74f313
Ehsan Javdan <ehsanjavdan77@gmail.com>
2026-06-02 23:26:02 +0400

    implemented showMe route

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -12,16 +12,14 @@ const getSingleUser = async (req, res) => {
   const user = await User.findOne({ _id: req.params.id }).select("-password");
 
   if (!user) {
-    throw new CustomError.NotFoundError(
-      `No user with id : ${req.params.id}`,
-    );
+    throw new CustomError.NotFoundError(`No user with id : ${req.params.id}`);
   }
 
   res.status(StatusCodes.OK).json({ user });
 };
 
 const showCurrentUser = async (req, res) => {
-  res.send("show current user");
+  res.status(StatusCodes.OK).json({ user: req.user });
 };
 
 const updateUser = async (req, res) => {
