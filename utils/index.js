7a3fc703737419e8f4a703568821d7a9e691087b
Ehsan Javdan <ehsanjavdan77@gmail.com>
2026-06-04 12:34:39 +0400

    added a new file called check permissions in the utils folder to check the permissions for get single user route

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,11 @@
 const { createJWT, isTokenValid, attachCookiesToResponse } = require("./jwt");
 const createTokenUser = require("./create_token_user");
+const checkPermissions = require("./check_permissions");
 
 module.exports = {
   createJWT,
   isTokenValid,
   attachCookiesToResponse,
   createTokenUser,
+  checkPermissions,
 };
