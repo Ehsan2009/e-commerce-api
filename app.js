d0bf20651e23c314a5499eb06b3db9a920f01ccb
Ehsan Javdan <ehsanjavdan77@gmail.com>
2026-06-07 16:13:45 +0400

    implemented upload image functionality in product controller

--- app.js
+++ app.js
@@ -5,34 +5,38 @@ const express = require("express");
 const app = express();
 
 // rest of the packages
-const morgan = require("morgan")
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
 
 // database
 const connectDB = require("./db/connect");
 
 // routers
-const authRouter = require("./routes/auth_routes")
-const userRouter = require("./routes/user_routes")
-const productRouter = require("./routes/product_routes")
+const authRouter = require("./routes/auth_routes");
+const userRouter = require("./routes/user_routes");
+const productRouter = require("./routes/product_routes");
 
 // middleware
 const notFoundMiddleware = require("./middleware/not_found");
 const errorHandlerMiddleware = require("./middleware/error_handler");
 
-app.use(morgan("tiny"))
+app.use(morgan("tiny"));
 app.use(express.json());
-app.use(cookieParser(process.env.JWT_SECRET))
-app.use(cors())
+app.use(cookieParser(process.env.JWT_SECRET));
+app.use(cors());
+
+app.use(express.static("./public"));
+app.use(fileUpload());
 
 app.get("/", (req, res) => {
   res.send("ecommerce api");
 });
 
-app.use("/api/v1/auth", authRouter)
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/products", productRouter)
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -41,7 +45,9 @@ const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
-    app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`),
+    );
   } catch (error) {
     console.log(error);
   }
