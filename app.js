517016ea8bebcbec3066b94112e4b40faddfd68f
Ehsan Javdan <ehsanjavdan77@gmail.com>
2026-06-08 22:00:52 +0400

    created review model and structured review controller and routes

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const connectDB = require("./db/connect");
 const authRouter = require("./routes/auth_routes");
 const userRouter = require("./routes/user_routes");
 const productRouter = require("./routes/product_routes");
+const reviewRouter = require("./routes/review_routes");
 
 // middleware
 const notFoundMiddleware = require("./middleware/not_found");
@@ -37,6 +38,7 @@ app.get("/", (req, res) => {
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
+app.use("/api/v1/reviews", reviewRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
